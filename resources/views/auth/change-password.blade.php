5fc9ea8af576f26f578b6f22b119a34626ffa07c
Reiaslite <raidearaujo06@gmail.com>
2026-07-21 11:01:05 +0700

    chore(auth): fixed code layout to improve readibility

--- resources/views/auth/change-password.blade.php
+++ resources/views/auth/change-password.blade.php
@@ -1,12 +1,17 @@
 @extends('layouts.cms')
 @section('title', 'Ganti kata sandi')
-@section('content')<div class="row justify-content-center">
+@section('content')
+    <div class="row justify-content-center">
         <div class="col-xl-7">
             <div class="cms-card p-4">
                 <h1 class="h2">Ganti kata sandi</h1>
-                <form method="post" action="{{ route('cms.password.update') }}">@csrf @method('PUT')<div class="mb-3">
+                <form method="post" action="{{ route('cms.password.update') }}">
+                    @csrf
+                    @method('PUT')
+                    <div class="mb-3">
                         <label class="form-label">Kata sandi saat ini</label><input class="form-control" type="password"
-                            name="current_password" required></div>
+                            name="current_password" required>
+                    </div>
                     <div class="mb-3"><label class="form-label">Kata sandi baru</label><input class="form-control"
                             type="password" name="password" required></div>
                     <div class="mb-4"><label class="form-label">Konfirmasi kata sandi</label><input class="form-control"
