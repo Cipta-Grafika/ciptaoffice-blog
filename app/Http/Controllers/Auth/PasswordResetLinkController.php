46d96639e509e1ea1562d1b708d38cb05249eea0
Reiaslite <raidearaujo06@gmail.com>
2026-07-22 09:00:41 +0700

    docs(auth): add PHPDoc documentation to Auth controllers

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -8,13 +8,27 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
 use Illuminate\View\View;
 
+/**
+ * Handle password reset link request emails.
+ */
 class PasswordResetLinkController extends Controller
 {
+    /**
+     * Display the password reset link request view.
+     *
+     * @return \Illuminate\View\View
+     */
     public function create(): View
     {
         return view('auth.forgot-password');
     }
 
+    /**
+     * Handle an incoming password reset link request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function store(Request $request): RedirectResponse
     {
         $request->validate(['email' => ['required', 'email']]);
