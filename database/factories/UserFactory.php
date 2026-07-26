5da59d522a59a0a40513622dfe447263be0a7214
Reiaslite <raidearaujo06@gmail.com>
2026-07-26 13:53:37 +0700

    fix(storage): remove empty post media directories and improve type inference

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,10 +3,14 @@
 namespace Database\Factories;
 
 use App\Enums\UserRole;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
+/**
+ * @extends Factory<User>
+ */
 class UserFactory extends Factory
 {
     protected static ?string $password;
