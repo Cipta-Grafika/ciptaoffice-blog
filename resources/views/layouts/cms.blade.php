b85ce11071b243b53d9b57d98fcb025098119bac
Reiaslite <raidearaujo06@gmail.com>
2026-07-28 14:28:41 +0700

    refactor(cms): remove global error alert from main layout

--- resources/views/layouts/cms.blade.php
+++ resources/views/layouts/cms.blade.php
@@ -216,15 +216,7 @@
             <div class="alert alert-success alert-dismissible fade show">{{ session('success') }}<button
                     class="btn-close" data-bs-dismiss="alert"></button></div>
         @endif
-        @if ($errors->any())
-            <div class="alert alert-danger"><strong>Periksa kembali:</strong>
-                <ul class="mb-0">
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
+
         @yield('content')
     </div>
     <x-cms-confirm-modal />
