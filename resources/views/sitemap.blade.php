1bf59af0894a24afd74e402cfb5b42fbd0b4c462
Reiaslite <raidearaujo06@gmail.com>
2026-07-29 12:00:15 +0700

    feat(seo): add priority, change frequency, and last modification metadata to sitemap entries
    - Added <priority>, <changefreq>, and <lastmod> tags to all URLs.
    - Restructured static routes into an array to configure specific SEO priorities (e.g., home 1.0, products 0.9).
    - Configured dynamic routes (posts and products) with weekly change frequencies and their respective priorities (0.7 and 0.8).

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,20 +1,36 @@
 {!! '<' . '?xml version="1.0" encoding="UTF-8"?>' !!}
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    @foreach ([route('home'), route('about'), route('products.index'), route('articles.index'), route('contact.create')] as $url)
+    @php
+        $staticPages = [
+            ['url' => route('home'), 'priority' => '1.0', 'changefreq' => 'daily'],
+            ['url' => route('about'), 'priority' => '0.8', 'changefreq' => 'monthly'],
+            ['url' => route('products.index'), 'priority' => '0.9', 'changefreq' => 'weekly'],
+            ['url' => route('articles.index'), 'priority' => '0.9', 'changefreq' => 'weekly'],
+            ['url' => route('contact.create'), 'priority' => '0.8', 'changefreq' => 'monthly'],
+        ];
+    @endphp
+    @foreach ($staticPages as $page)
         <url>
-            <loc>{{ $url }}</loc>
+            <loc>{{ $page['url'] }}</loc>
+            <lastmod>{{ now()->startOfDay()->toAtomString() }}</lastmod>
+            <changefreq>{{ $page['changefreq'] }}</changefreq>
+            <priority>{{ $page['priority'] }}</priority>
         </url>
     @endforeach
     @foreach ($posts as $post)
         <url>
             <loc>{{ route('articles.show', $post) }}</loc>
             <lastmod>{{ $post->updated_at->toAtomString() }}</lastmod>
+            <changefreq>weekly</changefreq>
+            <priority>0.7</priority>
         </url>
     @endforeach
     @foreach ($products as $product)
         <url>
             <loc>{{ route('products.show', $product) }}</loc>
             <lastmod>{{ $product->updated_at->toAtomString() }}</lastmod>
+            <changefreq>weekly</changefreq>
+            <priority>0.8</priority>
         </url>
     @endforeach
 </urlset>
